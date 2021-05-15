d7490cf03025f8bfb0f8c2c5cfd26fe7f6bcb7e3
RightToPrivacy <58191050+RightToPrivacy@users.noreply.github.com>
2021-05-15 16:43:38 -0400

    Create install.sh
    install as command

---
+++ install.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+#
+# install mnt-pi-img as command
+#
+# <righttoprivacy[at]@tutanota.com>
+#
+sudo cp mnt-pi-img /usr/bin/mnt-pi-img
+chmod +x /usr/bin/mnt-pi-img
+echo "mnt-pi-img command now ready to use."
+echo ' '
+echo "To use simply issue: sudo mnt-pi-img."
